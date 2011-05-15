b7b57a98d6b151fbccec5e28e8cd6a910ac8aa35
Britton Leo Kerin <britton.kerin@gmail.com>
2011-05-15 15:48:22 -0700

    Change to use potentiometer reading

--- lesson9/lesson9.c
+++ lesson9/lesson9.c
@@ -13,7 +13,19 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
  */
+
+// Assumptions:
+//
+// 	- 10 kohm (or so) potentiometer connected between 5V supply and ground,
+// 	  with potentionmeter tap connected to pin PC0 (ADC0).
+//
+// 	- Note that there are a variety of hardware techniques that can be used
+// 	  to improve the resolution and noise resistance of the ADC; the
+// 	  ATMega328P datasheet discusses these.  For simplicity, we assume 
+// 	  that they aren't needed here.
+
 
 #include <avr/io.h>
 #include <stdio.h>
@@ -21,91 +33,57 @@
 #include <util/delay.h>
 
 /* let the compiler do some of this, to avoid malloc */
-static int cput(char, FILE *);
-static FILE O = FDEV_SETUP_STREAM(cput, NULL, _FDEV_SETUP_WRITE);
+static int cput (char, FILE *);
+static FILE O = FDEV_SETUP_STREAM (cput, NULL, _FDEV_SETUP_WRITE);
 
-static int cput(char c, FILE *f)
+static int
+cput (char c, FILE *f)
 {
         f = f;   // Compiler reassurance.
   
-	loop_until_bit_is_set(UCSR0A, UDRE0);
+	loop_until_bit_is_set (UCSR0A, UDRE0);
 	UDR0 = c;
 	return 0;
 }
 
-/*
- * This lookup table and the adc_convert function are used to map raw
- * sensor values into more meaningful measurements. It's not necessary
- * to do analog input, but it sure make the output prettier. This table
- * is only valid for my input device - it was generated with RepRap's
- * createTemperatureLookup.py.
- */
-#define NUMTEMPS 21
-short T[NUMTEMPS][2] = { /* raw:temp (in oC * 10) */
-	{1, 8041},	{52, 2514},	{103, 2061},
-	{154, 1817},	{205, 1648},	{256, 1518},
-	{307, 1410},	{358, 1317},	{409, 1233},
-	{460, 1156},	{511, 1084},	{562, 1014},
-	{613, 946},	{664, 877},	{715, 806},
-	{766, 731},	{817, 650},	{868, 557},
-	{919, 441},	{970, 274},	{1021, -259}
-};
-
-static short adc_convert(short in)
+static void
+adc_init (void)
 {
-	uint8_t i;
-	short s, r = 0x8000;
+        // Internal pull-ups interfere with the ADC. disable the pull-up on the
+        // pin if it's being used for ADC. either writing 0 to the port
+        // register or setting it to output should be enough to disable
+        // pull-ups.
+        PORTC = 0x00;
+        DDRC = 0x00;
 
-	/* clamp the input range */
-	if (in < T[0][0])
-		in = T[1][0];
-	if (in > T[NUMTEMPS-1][0])
-		in = T[NUMTEMPS-1][0];
-
-	/* walk the table */
-	for(i = 0; i < NUMTEMPS-1; i++)
-		/* to find the conversion range to use */
-		if ((in >= T[i][0]) && (in < T[i+1][0])){
-			/* calculate the slope of this segment */
-			s = (T[i+1][1] - T[i][1]) / (T[i+1][0] - T[i][0]);
-			/* y = m x + b */
-			r = (in - T[i][0]) * s + T[i][1] ;
-		}
-	return r;
-}
-
-static void adc_init()
-{
-	/* internal pull-ups interfere with the ADC. disable the
-	 * pull-up on the pin if it's being used for ADC. either
-	 * writing 0 to the port register or setting it to output
-	 * should be enough to disable pull-ups. */
-	PORTC = 0x00;
-	DDRC = 0x00;
-	/* unless otherwise configured, arduinos use the internal Vcc
-	 * reference. MUX 0x0f samples the ground (0.0V). */
+        // Unless otherwise configured, arduinos use the internal Vcc
+        // reference. MUX 0x0f samples the ground (0.0V) (we'll change this
+        // before each actual ADC read).
 	ADMUX = _BV(REFS0) | 0x0f;
-	/*
-	 * Enable the ADC system, use 128 as the clock divider on a 16MHz
-	 * arduino (ADC needs a 50 - 200kHz clock) and start a sample. the
-	 * AVR needs to do some set-up the first time the ADC is used; this
-	 * first, discarded, sample primes the system for later use.
-	 */
+
+        // Enable the ADC system, use 128 as the clock divider on a 16MHz
+        // arduino (ADC needs a 50 - 200kHz clock) and start a sample.  The AVR
+        // needs to do some set-up the first time the ADC is used; this first,
+        // discarded, sample primes the system for later use.
 	ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0) | _BV(ADSC);
-	/* wait for the ADC to return a sample */
-	loop_until_bit_is_clear(ADCSRA, ADSC);
+
+	// Wait for the ADC to return a sample.
+	loop_until_bit_is_clear (ADCSRA, ADSC);
 }
 
-unsigned short adc_read(unsigned char pin)
+static unsigned short
+adc_read (unsigned char pin)
 {
 	unsigned char l, h, r;
 
 	r = (ADMUX & 0xf0) | (pin & 0x0f);
-	ADMUX = r; /* select the input channel */
-	ADCSRA |= _BV(ADSC);
-	loop_until_bit_is_clear(ADCSRA, ADSC);
 
-	/* must read the low ADC byte before the high ADC byte */
+        // Select the input channel.
+	ADMUX = r;
+	ADCSRA |= _BV(ADSC);
+	loop_until_bit_is_clear (ADCSRA, ADSC);
+
+        // It is required to read the low ADC byte before the high byte.
 	l = ADCL;
 	h = ADCH;
 
@@ -114,10 +92,10 @@ unsigned short adc_read(unsigned char pin)
 
 int main (void)
 {
-	short raw, tc, tf;
-	PGM_P fmtstr = "thermistor output: %d raw %d oC %d oF\r\n";
+	short raw;
+	PGM_P pot_fmtstr = "Potentiometer tap voltage: %f (%d raw)\r\n";
 
-	/* set up stdio */
+        // Set up stdio.
 #define BAUD 9600
 #include <util/setbaud.h>
 	UBRR0H = UBRRH_VALUE;
@@ -126,26 +104,19 @@ int main (void)
 	UCSR0B = _BV(RXEN0) | _BV(TXEN0);
 	stdout = &O;
 
-	adc_init(0);
-	DDRB = 0xff; /* there are some LEDs connected to PORTB */
+	adc_init ();
 
-	while (1) {
-		raw = adc_read(0);
+	while ( 1 )
+        {
+		raw = adc_read (0);
 
-		/* convert raw readings into more meaningful measurement */
-		tc = adc_convert(raw);
-		tf = ((tc * 9) / 5) +320;
-		printf(fmtstr, raw, tc/10, tf/10);
+                // Print tap voltage and raw ADC value.
+                const uint16_t a2d_steps = 1024;
+                const float supply_voltage = 5.0;
+                float tap_voltage = ((float) raw / a2d_steps) * supply_voltage;
+                printf (pot_fmtstr, tap_voltage, raw);
 
-		/* light up some LED depending on the temperature */
-		if (raw > 970)
-			PORTB = _BV(PORTB1);
-		else if ((raw <= 970) && (raw > 955))
-			PORTB = _BV(PORTB2);
-		else if (raw <= 955)
-			PORTB = _BV(PORTB3);
-
-		_delay_ms(500);
+		_delay_ms (500);
 	}
 	return 0;
 }
