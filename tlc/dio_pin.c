b0deb78924bdee85dd0d06330ec1b4853298312b
Britton Leo Kerin <britton.kerin@gmail.com>
2012-01-22 12:05:12 -0900

    Cleaner dio interface, needs tested

--- tlc/dio_pin.c
+++ tlc/dio_pin.c
@@ -1,17 +1,212 @@
 // Implementation of the interface described in dio_pin.h.
 
+#include <assert.h>
+#include <stdlib.h>
 #include <avr/io.h>
 
 #include "dio_pin.h"
 
+// Microcontrollers seem to use "PINx" to mean "register which receives
+// input" and "PORTx" to mean "register to which output (for output pins)
+// or pullup resistor status (for input pins) is written.
+
+#define PINB_REGISTER 0x03
+#define DDRB_REGISTER 0x04
+#define PORTB_REGISTER 0x05
+
+#define PINC_REGISTER 0x06
+#define DDRC_REGISTER 0x07
+#define PORTC_REGISTER 0x08
+
+#define PIND_REGISTER 0x09
+#define DDRD_REGISTER 0x0A
+#define PORTD_REGISTER 0x0B
+
 void
-dio_pin_initialize (char port, uint8_t pin, dio_pin_direction_t direction, 
+digital_io_pin_init (
+    digital_io_pin_name_t        pin,
+    digital_io_pin_direction_t   direction,
+    uint8_t                      enable_pullup,
+    uint8_t                      initial_value )
+{
+  uint8_t pin_register, dd_register, port_register;
+  uint8_t ppn;   // Port pin number (0-7).
+
+  // This code saves a bit of code and a few comparisons by depending on
+  // the order of the constants in the digital_io_pin_name_t enumeration
+  // (including the values that don't guarantee a particular value to the
+  // client).  So we make a few assertions out of paranoia.
+  assert (DIGITAL_IO_PIN_PB6 == 14); 
+  assert (DIGITAL_IO_PIN_PC5 == 21); 
+  if ( pin <= 7 ) {
+    pin_register = PIND_REGISTER;
+    dd_register = DDRD_REGISTER;
+    port_register = PORTD_REGISTER;
+    ppn = pin;
+  }
+  else if ( pin > 15 ) {
+    pin_register = PINC_REGISTER;
+    dd_register = DDRC_REGISTER;
+    port_register = PORTC_REGISTER;
+    ppn = pin - 16;
+  }
+  else {
+    pin_register = PINB_REGISTER;
+    dd_register = DDRB_REGISTER;
+    port_register = PORTB_REGISTER;
+    ppn = pin - 8;
+  }
+
+// FIXME: compare this for code size to above value comparer.
+//  switch ( DIGITAL_IO_PIN_NAME ) {
+//    case DIGITAL_IO_PIN_PB0:
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB1:
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB2 :
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB3 :
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB4:
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB5:
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB6:
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PB7:
+//      pin_register = PINB_REGISTER;
+//      dd_register = DDRB_REGISTER;
+//      port_register = PORTB_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD0:
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD1 :
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD2 :
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD3 :
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD4 :
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD5:
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD6 :
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PD7:
+//      pin_register = PIND_REGISTER;
+//      dd_register = DDRD_REGISTER;
+//      port_register = PORTD_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PC0:
+//      pin_register = PINC_REGISTER;
+//      dd_register = DDRC_REGISTER;
+//      port_register = PORTC_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PC1:
+//      pin_register = PINC_REGISTER;
+//      dd_register = DDRC_REGISTER;
+//      port_register = PORTC_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PC2:
+//      pin_register = PINC_REGISTER;
+//      dd_register = DDRC_REGISTER;
+//      port_register = PORTC_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PC3:
+//      pin_register = PINC_REGISTER;
+//      dd_register = DDRC_REGISTER;
+//      port_register = PORTC_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PC4:
+//      pin_register = PINC_REGISTER;
+//      dd_register = DDRC_REGISTER;
+//      port_register = PORTC_REGISTER;
+//      break;
+//    case DIGITAL_IO_PIN_PC5:
+//      pin_register = PINC_REGISTER;
+//      dd_register = DDRC_REGISTER;
+//      port_register = PORTC_REGISTER;
+//      break;
+//    default:
+//      assert (0);   // Shouldn't be here
+//  }
+
+  if ( direction == DIGITAL_IO_PIN_DIRECTION_INPUT ) {
+    _SFR_IO8 (dd_register) &= ~(_BV (ppn));
+    loop_until_bit_is_clear (_SFR_IO8 (dd_register), ppn);
+    if ( enable_pullup ) {
+      _SFR_IO8 (port_register) |= _BV (ppn);
+      loop_until_bit_is_set (_SFR_IO8 (port_register), ppn);
+    }
+    else {
+      _SFR_IO8 (port_register) &= ~(_BV (ppn));
+      loop_until_bit_is_clear (_SFR_IO8 (port_register), ppn);
+    }
+  }
+  else {
+    _SFR_IO8 (dd_register) |= _BV (pin);
+    loop_until_bit_is_set (_SFR_IO8 (dd_register), pin);
+    if ( initial_value ) {
+      _SFR_IO8 (port_register) |= _BV (pin);
+      loop_until_bit_is_set (_SFR_IO8 (port_register), pin);
+    }
+    else {
+      _SFR_IO8 (port_register) &= ~(_BV (pin));
+      loop_until_bit_is_clear (_SFR_IO8 (port_register), pin);
+    }
+  }
+}
+
+void
+dio_pin_initialize (char port, uint8_t pin, digital_io_pin_direction_t direction, 
   uint8_t enable_pullup, int8_t initial_value)
 {
   switch ( port ) {
     case 'B':
     case 'b':
-      if ( direction == DIO_PIN_DIRECTION_INPUT ) {
+      if ( direction == DIGITAL_IO_PIN_DIRECTION_INPUT ) {
         DDRB &= ~(_BV (pin));
         loop_until_bit_is_clear (DDRB, pin);
         if ( enable_pullup ) {
@@ -39,7 +234,7 @@ dio_pin_initialize (char port, uint8_t pin, dio_pin_direction_t direction,
 
     case 'C':
     case 'c':
-      if ( direction == DIO_PIN_DIRECTION_INPUT ) {
+      if ( direction == DIGITAL_IO_PIN_DIRECTION_INPUT ) {
         DDRC &= ~(_BV (pin));
         loop_until_bit_is_clear (DDRC, pin);
         if ( enable_pullup ) {
@@ -67,7 +262,7 @@ dio_pin_initialize (char port, uint8_t pin, dio_pin_direction_t direction,
 
     case 'D':
     case 'd':
-      if ( direction == DIO_PIN_DIRECTION_INPUT ) {
+      if ( direction == DIGITAL_IO_PIN_DIRECTION_INPUT ) {
         DDRD &= ~(_BV (pin));
         loop_until_bit_is_clear (DDRD, pin);
         if ( enable_pullup ) {
--- tlc/dio_pin.h
+++ tlc/dio_pin.h
@@ -1,26 +1,73 @@
 // Set input/output mode and pullup status of digitla IO pins, and set or
 // read their values.
 
-#ifndef DIO_PIN_H
-#define DIO_PIN_H
+#ifndef DIGITAL_IO_PIN_H
+#define DIGITAL_IO_PIN_H
 
 #include <inttypes.h>
 
+// WARNING: Not all the digital IO pins are available for use when using
+// an Arduino with the normal bootloader pin configuration.  PD0 and PD1
+// are set up for serial communication, and won't work as general digital
+// IO pins (at least without disturbing something else).
+//
+// The pin names we use are the unparenthesized pin labels from the
+// ATMega328p datasheet.  The assigned numbers are the digital pin numbers
+// printed on (at least some) arduino boards (so they can be used instead if
+// the user likes).  For the PB6:7 and PC0:5 pins, my arduino doesn't have
+// any specific digital IO-related mark, so these pins have no guaranteed
+// value in this enumeration.
 typedef enum {
-  DIO_PIN_DIRECTION_INPUT,
-  DIO_PIN_DIRECTION_OUTPUT
-} dio_pin_direction_t;
+  DIGITAL_IO_PIN_PB0 = 8, 
+  DIGITAL_IO_PIN_PB1 = 9,
+  DIGITAL_IO_PIN_PB2 = 10, 
+  DIGITAL_IO_PIN_PB3 = 11, 
+  DIGITAL_IO_PIN_PB4 = 12,
+  DIGITAL_IO_PIN_PB5 = 13,
+  DIGITAL_IO_PIN_PB6,
+  DIGITAL_IO_PIN_PB7,
+  DIGITAL_IO_PIN_PC0,
+  DIGITAL_IO_PIN_PC1,
+  DIGITAL_IO_PIN_PC2,
+  DIGITAL_IO_PIN_PC3,
+  DIGITAL_IO_PIN_PC4,
+  DIGITAL_IO_PIN_PC5,
+  DIGITAL_IO_PIN_PD0 = 0,
+  DIGITAL_IO_PIN_PD1 = 1, 
+  DIGITAL_IO_PIN_PD2 = 2, 
+  DIGITAL_IO_PIN_PD3 = 3, 
+  DIGITAL_IO_PIN_PD4 = 4, 
+  DIGITAL_IO_PIN_PD5 = 5,
+  DIGITAL_IO_PIN_PD6 = 6, 
+  DIGITAL_IO_PIN_PD7 = 7
+} digital_io_pin_name_t;
+
+typedef enum {
+  DIGITAL_IO_PIN_DIRECTION_INPUT,
+  DIGITAL_IO_PIN_DIRECTION_OUTPUT
+} digital_io_pin_direction_t;
 
 // Initialize pin of port for input or output (as per direction argument).
 // If the pin is configured for input, then enable_pullup determines whether
 // the internal pullup resistor is enabled.  If configured for ouput,
 // initial_value determines the initial value of the pin.
 void
-dio_pin_initialize (char port, uint8_t pin, dio_pin_direction_t direction, 
+digital_io_pin_init (
+    digital_io_pin_name_t        pin,
+    digital_io_pin_direction_t   direction,
+    uint8_t                      enable_pullup,
+    uint8_t                      initial_value );
+
+// Initialize pin of port for input or output (as per direction argument).
+// If the pin is configured for input, then enable_pullup determines whether
+// the internal pullup resistor is enabled.  If configured for ouput,
+// initial_value determines the initial value of the pin.
+void
+dio_pin_initialize (char port, uint8_t pin, digital_io_pin_direction_t direction, 
   uint8_t enable_pullup, int8_t initial_value);
 
 // Set output pin of port to value.
 void
 dio_pin_set (char port, uint8_t pin, uint8_t value);
 
-#endif // DIO_PIN_H
+#endif // DIGITAL_IO_PIN_H
