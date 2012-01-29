8c7561016d261328f1e1256e45a3beb009abc5ae
Britton Leo Kerin <britton.kerin@gmail.com>
2012-01-29 13:15:19 -0900

    Slight comment and format changes

--- util.h
+++ util.h
@@ -3,11 +3,12 @@
 #ifndef MYLIB_H
 #define MYLIB_H
 
+// WARNING: of course some contexts might understand things differently...
 #define HIGH 0x1
 #define LOW  0x0
 
-// WARNING: of course some pins might see things differently...
-#define TRUE 0x1
+// WARNING: of course some contexts might understand things differently...
+#define TRUE  0x1
 #define FALSE 0x0
 
 #endif // MYLIB_H
