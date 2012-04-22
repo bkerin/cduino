22140a53821167745fbbb3a209fcda3db4569bb9
Britton Leo Kerin <britton.kerin@gmail.com>
2012-04-22 10:49:33 -0800

    New tests of pull up disable using weak ground tie

--- dio/dio_test.c
+++ dio/dio_test.c
@@ -77,9 +77,21 @@ main (void)
   // acknowledging the fact.
 #ifndef UNDERSTAND_PB6_PB7_PC6_MACROS_UNTESTED
 #  error The PB6, PB7, and PC6 control macros are identical in form to the \
-   other macros in this interface, but have not been tested.  The Makefile \
-   for this module includes a line which may be uncommented to override \
-   this failure.
+   other macros in this interface, but have not been tested because the \
+   arduino hardware uses them for other purposes (see comments in dio.h for \
+   details).  The Makefile for this module includes a line which may be \
+   uncommented to override this error trap.
+#endif
+
+// Its a sin in my book to distribute untested code without clearly
+// acknowledging the fact.
+#ifndef UNDERSTAND_PB5_PD0_PD1_WEAK_LOW_470_KOHM_CONDITIONS_UNTESTED
+#  error It is not possible to test the operation of PB5, PD0, or PD1 with a \
+   weak tie to ground with arduino hardware, because those pins are connected \
+   to ground by other paths. So the pullup control on these pins is somewhat \
+   untested.  I think this is ok since all the other pins are tested in \
+   highly similar code.  The Makefile for this module includes a line which \
+   may be uncommented to override this error trap.
 #endif
 
 // We're going to be a bit careless with out lack of namespace prefixes
@@ -95,10 +107,9 @@ main (void)
 #endif
 
   // The time we allow to give the pull-up resistors a chance to get their
-  // pins pulled high.  Note that in practice they shouldn't need a full this
-  // long to settle, at least with the anticipated capacitances connected
-  // to them.  But the chip runs fast enough that not waiting at all can be
-  // to fast.
+  // pins pulled high.  Note that in practice they shouldn't need a this long
+  // to settle, at least with the anticipated capacitances connected to them.
+  // But the chip runs fast enough that not waiting at all can be too fast.
   const double settling_time_ms __attribute__ ((unused)) = 10;
 
   uint8_t value = 0;   // Holds value set to or read from a pin.
@@ -219,77 +230,90 @@ main (void)
 
   // Macros for pin INIT/SET/READ depending on which TEST_CONDITION_* is
   // in effect.
-// {{{1
+  // {{{1
 
 #if defined(TEST_CONDITION_PB0_HIGH_OTHERS_NC) || \
     defined(TEST_CONDITION_PB0_LOW_OTHERS_NC) || \
+    defined(TEST_CONDITION_PB0_WEAK_LOW_470_KOHM_OTHERS_NC) || \
     defined(TEST_CONDITION_PB0_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PB0 
 #  define SET DIO_SET_PB0 
 #  define READ DIO_READ_PB0 
 #elif defined(TEST_CONDITION_PB1_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PB1_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PB1_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PB1_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PB1 
 #  define SET DIO_SET_PB1 
 #  define READ DIO_READ_PB1 
 #elif defined(TEST_CONDITION_PB2_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PB2_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PB2_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PB2_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PB2 
 #  define SET DIO_SET_PB2 
 #  define READ DIO_READ_PB2 
 #elif defined(TEST_CONDITION_PB3_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PB3_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PB3_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PB3_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PB3 
 #  define SET DIO_SET_PB3 
 #  define READ DIO_READ_PB3 
 #elif defined(TEST_CONDITION_PB4_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PB4_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PB4_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PB4_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PB4 
 #  define SET DIO_SET_PB4 
 #  define READ DIO_READ_PB4 
-#elif defined(TEST_CONDITION_PB5_HIGH_PB0_SIGNAL_LED_OTHERS_NC) || \
-      defined(TEST_CONDITION_PB5_LOW_PB0_SIGNAL_LED_OTHERS_NC) || \
-      defined(TEST_CONDITION_PB5_LED_OTHERS_NC)
+#elif \
+  defined(TEST_CONDITION_PB5_HIGH_PB0_SIGNAL_LED_OTHERS_NC) || \
+  defined(TEST_CONDITION_PB5_LOW_PB0_SIGNAL_LED_OTHERS_NC) || \
+  defined(TEST_CONDITION_PB5_WEAK_LOW_470_KOHM_PB0_SIGNAL_LED_OTHERS_NC) || \
+  defined(TEST_CONDITION_PB5_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PB5 
 #  define SET DIO_SET_PB5 
 #  define READ DIO_READ_PB5 
 
 #elif defined(TEST_CONDITION_PC0_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PC0_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PC0_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PC0_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PC0 
 #  define SET DIO_SET_PC0 
 #  define READ DIO_READ_PC0 
 #elif defined(TEST_CONDITION_PC1_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PC1_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PC1_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PC1_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PC1 
 #  define SET DIO_SET_PC1 
 #  define READ DIO_READ_PC1 
 #elif defined(TEST_CONDITION_PC2_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PC2_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PC2_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PC2_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PC2 
 #  define SET DIO_SET_PC2 
 #  define READ DIO_READ_PC2 
 #elif defined(TEST_CONDITION_PC3_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PC3_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PC3_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PC3_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PC3 
 #  define SET DIO_SET_PC3 
 #  define READ DIO_READ_PC3 
 #elif defined(TEST_CONDITION_PC4_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PC4_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PC4_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PC4_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PC4 
 #  define SET DIO_SET_PC4 
 #  define READ DIO_READ_PC4 
 #elif defined(TEST_CONDITION_PC5_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PC5_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PC5_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PC5_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PC5 
 #  define SET DIO_SET_PC5 
@@ -297,55 +321,63 @@ main (void)
 
 #elif defined(TEST_CONDITION_PD0_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD0_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD0_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD0_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD0 
 #  define SET DIO_SET_PD0 
 #  define READ DIO_READ_PD0 
 #elif defined(TEST_CONDITION_PD1_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD1_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD1_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD1_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD1 
 #  define SET DIO_SET_PD1 
 #  define READ DIO_READ_PD1 
 #elif defined(TEST_CONDITION_PD2_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD2_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD2_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD2_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD2 
 #  define SET DIO_SET_PD2 
 #  define READ DIO_READ_PD2 
 #elif defined(TEST_CONDITION_PD3_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD3_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD3_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD3_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD3 
 #  define SET DIO_SET_PD3 
 #  define READ DIO_READ_PD3 
 #elif defined(TEST_CONDITION_PD4_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD4_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD4_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD4_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD4 
 #  define SET DIO_SET_PD4 
 #  define READ DIO_READ_PD4 
 #elif defined(TEST_CONDITION_PD5_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD5_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD5_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD5_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD5 
 #  define SET DIO_SET_PD5 
 #  define READ DIO_READ_PD5 
 #elif defined(TEST_CONDITION_PD6_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD6_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD6_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD6_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD6 
 #  define SET DIO_SET_PD6 
 #  define READ DIO_READ_PD6 
 #elif defined(TEST_CONDITION_PD7_HIGH_OTHERS_NC) || \
       defined(TEST_CONDITION_PD7_LOW_OTHERS_NC) || \
+      defined(TEST_CONDITION_PD7_WEAK_LOW_470_KOHM_OTHERS_NC) || \
       defined(TEST_CONDITION_PD7_LED_OTHERS_NC)
 #  define INIT DIO_INIT_PD7 
 #  define SET DIO_SET_PD7 
 #  define READ DIO_READ_PD7 
 #endif
 
-// }}}1
+  // }}}1
 
   // Macros describing particular condition of pin being tested, depending
   // on which TEST_CONDITION_* is in effect.
@@ -392,6 +424,27 @@ main (void)
     defined(TEST_CONDITION_PD6_LOW_OTHERS_NC) || \
     defined(TEST_CONDITION_PD7_LOW_OTHERS_NC)
 #  define INPUT_LOW
+#elif defined(TEST_CONDITION_PB0_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PB1_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PB2_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PB3_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PB4_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PB5_WEAK_LOW_470_KOHM_PB0_SIGNAL_LED_OTHERS_NC) || \
+    defined(TEST_CONDITION_PC0_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PC1_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PC2_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PC3_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PC4_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PC5_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD0_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD1_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD2_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD3_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD4_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD5_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD6_WEAK_LOW_470_KOHM_OTHERS_NC) || \
+    defined(TEST_CONDITION_PD7_WEAK_LOW_470_KOHM_OTHERS_NC)
+#  define INPUT_WEAK_LOW_470_KOHM
 #elif defined(TEST_CONDITION_PB0_LED_OTHERS_NC) || \
     defined(TEST_CONDITION_PB1_LED_OTHERS_NC) || \
     defined(TEST_CONDITION_PB2_LED_OTHERS_NC) || \
@@ -451,10 +504,29 @@ main (void)
 #    error Should not be here
 #  endif
 
+#elif defined(INPUT_WEAK_LOW_470_KOHM)
+
+  // NOTE: these tests depend on the pin in question being connected as
+  // indicated by the test condition name.
+
+  // Test pin as input with pull-up enabled.
+  INIT (DIO_INPUT, DIO_ENABLE_PULLUP, DIO_DONT_CARE); 
+  _delay_ms (settling_time_ms);
+
+  value = READ ();
+  assert (value == HIGH);
+
+  // Test pin again as input but this time with pull-up disabled.
+  INIT (DIO_INPUT, DIO_DISABLE_PULLUP, DIO_DONT_CARE); 
+  _delay_ms (settling_time_ms);
+  
+  value = READ ();
+  assert (value == LOW);
+
 #elif defined(OUTPUT_LED)
 
   // NOTE: these tests depend on a LED being connected in the manner indicated
-  // by the test condition name and also no careful human observation of
+  // by the test condition name and also on careful human observation of
   // the LED during the test.
 
   const float second_in_ms = 1000.0;
