835f3199b1b957b65de9323d916a18dd1836c572
Britton Leo Kerin <britton.kerin@gmail.com>
2012-05-22 12:11:04 -0800

    Link in LCD for devel purposes

---
+++ sd_card/lcd_keypad.h
@@ -0,0 +1,1 @@
+../lcd_keypad/lcd_keypad.h
--- sd_card/sd_card_test.cpp
+++ sd_card/sd_card_test.cpp
@@ -1,7 +1,17 @@
 #include <Sd2Card.h>
 
+#ifdef __cplusplus
+extern "C" {
+  #include <lcd_keypad.h>
+}
+#endif
+
 int
 main (void)
 {
   Sd2Card card;
+
+  double card_size = (double) card.cardSize ();
+
+  lcd_keypad_show_value ("card_size", &card_size);
 }
