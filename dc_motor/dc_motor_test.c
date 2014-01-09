1763b0cf98cc3e27fd79d8c11c54c7d083e99f78
Britton Leo Kerin <britton.kerin@gmail.com>
2014-01-09 10:06:12 -0900

    Test both channels (in opposite directions)

--- dc_motor/dc_motor_test.c
+++ dc_motor/dc_motor_test.c
@@ -1,13 +1,32 @@
 // Test/demo for the dc_motor.h interface.
 
+// Assumptions:
+//
+//      - Arduino Motor Shield Rev. 3 plugged into Arduino
+//
+//      - Small 5V DC motor(s) connected to one (or both) shield outputs
+//
+//      - In my experience most small motors will turn when the Arduino is
+//        powered from USB, but for larger motors it may be necessary
+//        to power either the Arduino or the motor shield with a
+//        wall wart supply.  Read the material regarding power at
+//        http://arduino.cc/en/Main/ArduinoMotorShieldR3.
+//
+// This program will slowly ramp the motor speeds up and down (including
+// putting them in reverse).  The motors are run in opposite directions.
+// The load is measured at each speed and printed to the terminal (use
+// 'make run_screen' from the shell to view it).  Loading the motor shaft
+// (say, by pinching it slightly between thumb and forefinger... carefully)
+// should cause the load current to rise.  Note that at lower speed setting
+// and voltages, many motors won't get going, so there may be several steps
+// where the motors don't move.
+
 #include <util/delay.h>
 
 #include "dc_motor.h"
 #define TERM_IO_POLLUTE_NAMESPACE_WITH_DEBUGGING_GOOP
 #include "term_io.h"
 #include "util.h"
-
-// FIXME: document what this test requires/does in the usual spots
 
 int
 main (void)
@@ -25,9 +44,10 @@ main (void)
 
   PFP ("\n");
 
-  // Ramp motor speed up and down continually, measuring load at each step
+  // Ramp motor speeds up and down continually, measuring load at each step.
+  // Motors A and B will be run in opposite directions.
   {
-    int8_t spd = 0;          // Speed setting
+    int8_t spd = 0;          // Speed setting (for motor A)
     int8_t ss = 20;          // Step size (in % duty cycle)
     int8_t ssgn = 1;         // Step sign
     double tps = 2000.0;     // Time Per Step (in ms)
@@ -36,16 +56,20 @@ main (void)
 
     for ( ; ; ) {
 
-      //dc_motor_set_speed (DC_MOTOR_CHANNEL_B, ss * sign);
-      dc_motor_set_speed (DC_MOTOR_CHANNEL_B, spd);
-      PFP ("Set motor speed to %hhi\n", spd);
+      dc_motor_set_speed (DC_MOTOR_CHANNEL_A, spd);
+      PFP ("Set motor A speed to %hhi\n", spd);
+      dc_motor_set_speed (DC_MOTOR_CHANNEL_B, -spd);
+      PFP ("Set motor B speed to %hhi\n", -spd);
 
-      // Run motor at this speed for tps milliseconds, measuring load current
-      // half way through.
+      // Run motors at this speed (in opposite directions) for tps
+      // milliseconds, measuring load current half way through.
       _delay_ms (tps / 2.0);
       int mapa = 1000;   // Milliamps Per Amp
       PFP (
-          "dc motor load current: %i mA\n", 
+          "DC motor A load current: %i mA\n", 
+          (int) (mapa * dc_motor_load_current (DC_MOTOR_CHANNEL_A)) );
+      PFP (
+          "DC motor B load current: %i mA\n", 
           (int) (mapa * dc_motor_load_current (DC_MOTOR_CHANNEL_B)) );
       _delay_ms (tps / 2.0);
 
@@ -54,11 +78,11 @@ main (void)
       // Calculate motor speed to set on next iteration
       spd += ssgn * ss;
       if ( spd > max_spd ) {
-        spd = max_spd - ss;
+        spd = max_spd;
         ssgn = -1;
       }
-      if ( spd < min_spd ) {
-        spd = min_spd + ss;
+      else if ( spd < min_spd ) {
+        spd = min_spd;
         ssgn = 1;
       }
     }
