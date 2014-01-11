353588ff9cacb46b4f73ae4006ae456b4c8ca7f9
Britton Leo Kerin <britton.kerin@gmail.com>
2014-01-10 20:14:40 -0900

    Moved a uart test over to uart module

--- uart/uart_test.c
+++ uart/uart_test.c
@@ -36,61 +36,97 @@ FILE uart_str = FDEV_SETUP_STREAM (uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 int
 main (void)
 {
-  char buf[20], s[20];
 
   uart_init ();
 
+  assert (sizeof (char) == 1);   // Hey, I like probably correct programs :)
+
+#define CHARS_TO_READ 5
+
+  char ptec[] = "\n\rType some characters now\n\r";   // Prompt To Enter Chars
+  char ce[CHARS_TO_READ];   // Characters entered
+
+  // Put a prompt on the wire
+  for ( uint8_t ii = 0 ; ii < sizeof (ptec) ; ii++ ) {
+    uart_put_byte (ptec[ii]);
+  }
+
+  // Read the characters entered
+  for ( uint8_t ii = 0 ; ii < CHARS_TO_READ ; ii++ ) {
+    if ( UART_RX_ERROR () ) {
+      // Shouldn't be here.  We could try to print an error...
+      assert (0);
+    }
+    ce[ii] = uart_get_byte();
+  }
+
+  char ebp[] = "You entered these characters: ";   // Echo Back Prefix
+
+  for ( uint8_t ii = 0 ; ii < sizeof (ebp) ; ii++ ) {
+    uart_put_byte (ebp[ii]);
+  }
+
+  for ( uint8_t ii = 0 ; ii < CHARS_TO_READ ; ii++ ) {
+    uart_put_byte (ce[ii]);
+  }
+
+  uart_put_byte ('\n');
+  uart_put_byte ('\n');
+
+  // FIXME: this stuff should move over to the term_io module
+
+  char buf[20], s[20];
+
   stdout = stdin = &uart_str;
 
-  for (;;)
-    {
-      printf_P(PSTR("\nAVR Ready.\nEnter command: "));
-      if (fgets(buf, sizeof buf - 1, stdin) == NULL)
-	break;
-      if (tolower(buf[0]) == 'q')
-	break;
+  for ( ; ; ) {
 
-      switch (tolower(buf[0]))
-	{
-	default:
-	  printf(
-              "Unknown command: '%c'\n\n"
-              "'u some_string' is the only working command at present\n",
-              buf[0]);
-	  break;
+    printf_P (PSTR ("\nAVR Ready.\nEnter command: "));
 
-        case '\n':
-          break;
-
-	case 'l':
-	  if (sscanf(buf, "%*s %s", s) > 0)
-	    {
-	      printf("AAAACCCckkkk we can't do that (LCD unimplemented)\n");
-	    }
-	  else
-	    {
-	      printf("sscanf() failed\n");
-	    }
-	  break;
-
-	case 'u':
-	  if (sscanf(buf, "%*s %s", s) > 0)
-	    {
-	      fprintf(&uart_str, "Got %s\n", s);
-	      printf("OK\n");
-	    }
-	  else
-	    {
-              if (sscanf(buf, "u ") == 0) {
-                fprintf(&uart_str, "Got 'u' command without an argument\n");
-              } 
-              else {
-	        printf("sscanf() failed\n");
-              }
-	    }
-	  break;
-	}
+    if ( fgets (buf, sizeof buf - 1, stdin) == NULL) {
+      break;
     }
+    if ( tolower (buf[0]) == 'q' ) {
+      break;
+    }
+
+    switch ( tolower (buf[0]) ) {
+
+      default:
+        printf (
+            "Unknown command: '%c'\n\n"
+            "'u some_string' is the only working command at present\n",
+            buf[0] );
+        break;
+
+      case '\n':
+        break;
+
+      case 'l':
+        if ( sscanf(buf, "%*s %s", s) > 0 ) {
+          printf("AAAACCCckkkk we can't do that (LCD unimplemented)\n");
+        }
+        else {
+          printf("sscanf() failed\n");
+        }
+        break;
+
+      case 'u':
+        if ( sscanf (buf, "%*s %s", s) > 0) {
+          fprintf (&uart_str, "Got %s\n", s);
+          printf ("OK\n");
+        }
+        else {
+          if ( sscanf (buf, "u ") == 0 ) {
+            fprintf (&uart_str, "Got 'u' command without an argument\n");
+          } 
+          else {
+            printf ("sscanf() failed\n");
+          }
+        }
+        break;
+    }
+  }
 
   return 0;
 }
--- wireless_xbee/wireless_xbee_test.c
+++ wireless_xbee/wireless_xbee_test.c
@@ -7,32 +7,7 @@ main (void)
 {
   uart_init ();
 
-  // FIXME: this stuff should be moved over to the uart module
+  // FIXME: WORK POINT: The basic back-and-forth got tested n uart module now,
+  // now we need to think about how to start testing the xbee itself here.
 
-#define CHARS_TO_GET 5
-  uint8_t received_chars[CHARS_TO_GET];
-  uint8_t ii;
-
-  for ( ii = 0 ; ii < CHARS_TO_GET ; ii++ ) {
-    if ( UART_RX_ERROR () ) {
-      exit (1);
-    }
-    else {
-      received_chars[ii] = uart_get_byte ();
-    }
-  }
-
-  uart_put_byte ('b');
-  uart_put_byte ('o');
-  uart_put_byte ('o');
-  uart_put_byte ('g');
-  uart_put_byte ('e');
-  uart_put_byte ('r');
-  uart_put_byte (':');
-  uart_put_byte (' ');
-  for ( ii = 0 ; ii < CHARS_TO_GET ; ii++ ) {
-    uart_put_byte (received_chars[ii]);
-  }
-  uart_put_byte ('\r');
-  uart_put_byte ('\n');
 }
