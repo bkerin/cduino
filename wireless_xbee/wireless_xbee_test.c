fa9e1bc5d3e70149d085d33a8fa6fa4a8f8353fc
Britton Leo Kerin <britton.kerin@gmail.com>
2014-01-12 13:43:58 -0900

    Working get to command mode

--- wireless_xbee/wireless_xbee_test.c
+++ wireless_xbee/wireless_xbee_test.c
@@ -1,13 +1,37 @@
-#include <stdlib.h>
+#include <assert.h>
+#include <stdlib.h>  // FIXME: probably only needed for broken assert.h 
+#include <util/delay.h>
 
 #include "uart.h"
+#include "util.h"
   
 int
 main (void)
 {
   uart_init ();
 
-  // FIXME: WORK POINT: The basic back-and-forth got tested n uart module now,
-  // now we need to think about how to start testing the xbee itself here.
+  // Sequence to go into command mode
+  _delay_ms (1200);
+  UART_PUT_BYTE ('+');
+  UART_PUT_BYTE ('+');
+  UART_PUT_BYTE ('+');
+  _delay_ms (1200);
+    
+  // Get a byte
+  UART_WAIT_FOR_BYTE ();
+  assert (! UART_RX_ERROR ());
+  assert (sizeof (char) == 1);
+  char b1 = UART_GET_BYTE ();
+
+  // Get another byte
+  UART_WAIT_FOR_BYTE ();
+  assert (! UART_RX_ERROR ());
+  assert (sizeof (char) == 1);
+  char b2 = UART_GET_BYTE ();
+
+  // See if we got OK indicating command mode
+  if ( b1 == 'O' && b2 == 'K' ) {
+    for ( ; ; ) { CHKP (); }
+  }
 
 }
